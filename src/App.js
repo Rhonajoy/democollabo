3bad0d2419dbb91c7a543fdcbe39ea484749c13b
ErickMose <erickmongaremose@gmail.com>
2023-05-31 19:44:36 +0300

    feat: add index file in footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import About from "./components/about/About";
-import Footer from "./components/footer/Footer";
+import { Footer } from "./components/footer";
 
 function App() {
   return (
